e46d870847c4f0b5708fa842ba652dd06abdbe60
Durga Prasad <saikamireddi3@gmail.com>
2025-05-10 12:25:10 +0530

    useEffect changed

--- src/components/SumCalculator.jsx
+++ src/components/SumCalculator.jsx
@@ -1,20 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 const SumCalculator = () => {
-  const [Value, setValue] = useState(0);
+  const [nums, setNums] = useState([]);
   const [sum, setSum] = useState(0);
   const handleChange = (e) => {
-    let inputval = e.target.value;
-    setValue(inputval);
-    let num = parseInt(inputval, 10);
-    if (!isNaN(num)) {
-      setSum(sum + num);
+    let inputval = parseInt(e.target.value, 10);
+    if (!isNaN(inputval)) {
+      setNums((prev) => [...prev, inputval]);
     }
   };
+  useEffect(() => {
+    const total = nums.reduce((acc, val) => acc + val, 0);
+    setSum(total);
+  }, [nums]);
   return (
     <div>
       <h1>Sum Calculator</h1>
-      <input type="number" value={Value} onChange={handleChange} />
+      <input type="number" onChange={handleChange} />
       <p>Sum:{sum}</p>
     </div>
   );
